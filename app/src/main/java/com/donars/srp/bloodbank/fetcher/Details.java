3ddc12ac8200fb152542c435a906b42f1c457bb4
ragu <raguraman1703@gmail.com>
2017-03-26 14:57:42 +0530

    Initial commit

---
+++ app/src/main/java/com/donars/srp/bloodbank/fetcher/Details.java
@@ -0,0 +1,20 @@
+package com.donars.srp.bloodbank.fetcher;
+
+import com.donars.srp.bloodbank.model.Hospital;
+import com.donars.srp.bloodbank.model.User;
+import com.donars.srp.bloodbank.model.UserStats;
+
+/**
+ * Created by Lakshman on 10/12/2016.
+ */
+
+public class Details {
+    public static Hospital hospital;
+    public static User user;
+    public static UserStats userinfo;
+    public static int status=0;
+    //o inital
+    //1 counter
+    //2 acc
+    //3 rej
+}
